320aee21fd0b28bf57547f75ab33584f4c218269
a1l1ke <onetenthplease@gmail.com>
2026-06-04 14:16:10 +0900

    feat: 정적 파일(/public) 설정 및 복합 /chat 라우트 구현 시작

--- 04_deploy.js
+++ 04_deploy.js
@@ -1,6 +1,7 @@
 // 의존성
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const { GoogleGenAI } = require("@google/genai");
 const GroqAI = require("groq-sdk");
 
@@ -11,6 +12,22 @@ const google = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 const groq = new GroqAI({ apiKey: GROQ_API_KEY });
 
 app.use(express.json());
+
+// 파일들에 대한 접근을 /public에 대해서 열어두겠다 (/public은 제외한 뒤에 경로들)
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/chat", async (req, res) => {
+  // 입력 (JSON)
+  const { provider, model, ask } = req.body;
+  // 로직 (AI Provider)
+
+  // 출력 (JSON)
+  res.json({
+    provider,
+    model,
+    ask,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`${PORT}에서 실행`);
