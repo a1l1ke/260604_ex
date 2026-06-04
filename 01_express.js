b4683d2b20687753ea69405ef7e83da82a2f9a23
a1l1ke <onetenthplease@gmail.com>
2026-06-04 09:50:10 +0900

    feat: Express 기본 서버 설정 및 포트 바인딩

---
+++ 01_express.js
@@ -0,0 +1,12 @@
+// commonjs es-module
+const express = require("express");
+// 자동으로 안잡히면 터미널에서 `npm i express`를 안한 것
+
+const app = express(); // 호출
+
+// 이미 실행 중인 3000 포트가 있다면 3001이나 3333 등 대체 포트로 실행
+app.listen(3000, () => {
+  console.log("3000에서 서버 실행 중");
+});
+// 터미널에
+// node 01_express.js
